5d7a4c160c420eccb119035d527dbb7c979a554d
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-07-29 12:38:21 +0200

    Optimize modal image loading

--- js/modal-offer.js
+++ js/modal-offer.js
@@ -1,6 +1,16 @@
 import { openConnectModal } from "./modal-contact.js"
 import { modalData } from "./data.js"
 
+function preloadModalImages() {
+	modalData.forEach(service => {
+		const img = new Image();
+
+		img.src = service.image + ".webp";
+		img.srcset = `${service.image}.webp 1x, ${service.image}@2x.webp 2x`;
+	});
+}
+
+preloadModalImages();
 
 
 export const modalOfferOverlay = document.querySelector(".modal-offer");
@@ -16,40 +26,50 @@ const modalOfferFeaturesTitle = modalOfferContainer.querySelector(".modal-featur
 const modalOfferList = modalOfferContainer.querySelector(".modal-list");
 const offerItems = document.querySelectorAll(".offer-item");
 
-export function openOfferModal(service) {
-  modalOfferImage.srcset = `${service.image}.webp 1x, ${service.image}@2x.webp 2x`
-  modalOfferImage.src = service.image + ".webp";
-  modalOfferImage.alt = "Image that represents " + service.title.toLocaleLowerCase();
-  modalOfferTitle.textContent = service.title;
-  modalOfferSubtitle.textContent = service.subtitle;
-  modalOfferDescription.textContent = service.description;
-  modalOfferFeaturesTitle.textContent = service.featuresTitle;
-  modalOfferList.innerHTML = generateFeatures(service.features);
 
-  modalOfferOverlay.classList.add("modal-open");
-  document.body.classList.add("body-not-scroll")
+export async function openOfferModal(service) {
+	// Image handling ↓
+	modalOfferImage.classList.toggle("opacity-0");
+//--|--
+		modalOfferImage.srcset = `${service.image}.webp 1x, ${service.image}@2x.webp 2x`;
+		modalOfferImage.src = service.image + ".webp";
+		modalOfferImage.alt = "Image that represents " + service.title.toLowerCase();
+
+		await modalOfferImage.decode();
+//--|--
+	modalOfferImage.classList.toggle("opacity-0");
+	// Image handled ↑
+
+	modalOfferTitle.textContent = service.title;
+	modalOfferSubtitle.textContent = service.subtitle;
+	modalOfferDescription.textContent = service.description;
+	modalOfferFeaturesTitle.textContent = service.featuresTitle;
+	modalOfferList.innerHTML = generateFeatures(service.features);
+
+	modalOfferOverlay.classList.add("modal-open");
+	document.body.classList.add("body-not-scroll")
 }
 
 export function closeModal() {
-  modalOfferOverlay.classList.remove("modal-open");
-  document.body.classList.remove("body-not-scroll");
+	modalOfferOverlay.classList.remove("modal-open");
+	document.body.classList.remove("body-not-scroll");
 }
 
 function generateFeatures(features) {
-  return (
-    features.map(feature =>
-      `<li class="modal-item border border-green rounded-[200px] bg-transparent px-4 w-full font-inter font-normal text-sm leading-normal tracking-[-0.01em] text-green desktop:w-fit desktop:base">${feature}</li>`).join("")
-  )
+	return (
+		features.map(feature =>
+			`<li class="modal-item border border-green rounded-[200px] bg-transparent px-4 w-full font-inter font-normal text-sm leading-normal tracking-[-0.01em] text-green desktop:w-fit desktop:base">${feature}</li>`).join("")
+	)
 }
 
 modalOfferCrossButton.addEventListener("click", closeModal);
 modalOfferContactButton.addEventListener("click", () => {
-  closeModal();
-  openConnectModal();
+	closeModal();
+	openConnectModal();
 })
 
 offerItems.forEach(item => {
-  item.addEventListener("click", function () {
-    openOfferModal(modalData[+item.dataset.number]);
-  });
+	item.addEventListener("click", function () {
+		openOfferModal(modalData[+item.dataset.number]);
+	});
 });
